4c40e51434839ba70840c032c5d610aee207b154
Zdravko Galchov <zdravko.galchov@gmail.com>
2024-11-25 16:25:25 +0000

    Added another solution

---
+++ 06_exam_preparation/02_guards.py
@@ -0,0 +1,34 @@
+"""
+Given a directed graph and a start vertex, the output will be all vertices,
+that cannot be reached.
+
+Simple solution, using Depth-First Search algorithm.
+"""
+
+
+def dfs(node, graph, visited):
+    if node in visited:
+        return
+
+    visited.add(node)
+    for child in graph[node]:
+        dfs(child, graph, visited)
+
+
+nodes = int(input())
+edges = int(input())
+
+graph = {node: [] for node in range(nodes + 1)}
+
+for _ in range(edges):
+    source, destination = [int(x) for x in input().split()]
+    graph[source].append(destination)
+
+start_node = int(input())
+
+visited = set()
+
+dfs(start_node, graph, visited)
+
+unreachable_nodes = [node for node in graph if node not in visited and node > 0]
+print(*unreachable_nodes, sep=' ')
